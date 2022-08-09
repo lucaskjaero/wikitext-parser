72cbef22034d9dd60f7ca53638a012de720ae190
Lucas Kjaero <lucas@lucaskjaero.com>
2022-08-10 00:39:23 +0200

    Give me some types

--- lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/objects/Section.java
+++ lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/objects/Section.java
@@ -1,8 +1,14 @@
 package com.lucaskjaerozhang.wikitext_parser.objects;
 
-public class Section implements WikiTextNode {
+import java.util.List;
+
+public record Section(
+    String title, Integer level, List<WikiTextNode> content, List<Section> subsections)
+    implements WikiTextNode {
+  public static final String TYPE = "Section";
+
   @Override
   public String getType() {
-    return "Section";
+    return TYPE;
   }
 }
--- lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/parse/WikitextVisitor.java
+++ lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/parse/WikitextVisitor.java
@@ -6,18 +6,93 @@ import com.lucaskjaerozhang.wikitext_parser.objects.Article;
 import com.lucaskjaerozhang.wikitext_parser.objects.Section;
 import com.lucaskjaerozhang.wikitext_parser.objects.WikiTextNode;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WikitextVisitor extends WikiTextBaseVisitor<WikiTextNode> {
+
   @Override
-  public WikiTextNode visitRoot(WikiTextParser.RootContext ctx) {
+  public Article visitRoot(WikiTextParser.RootContext ctx) {
     List<Section> sections =
-        ctx.sectionContent().stream()
-            .map(node -> (Section) visit(node))
-            .collect(Collectors.toList());
+        ctx.sectionContent().stream().map(node -> (Section) visit(node)).toList();
 
     Article article = new Article();
     article.setSections(sections);
+
     return article;
+  }
+
+  @Override
+  public Section visitSectionLevelOne(WikiTextParser.SectionLevelOneContext ctx) {
+    List<WikiTextNode> content = ctx.sectionOneContent().stream().map(this::visit).toList();
+    List<Section> subsections =
+        content.stream()
+            .filter(c -> c.getType().equals(Section.TYPE))
+            .map(Section.class::cast)
+            .toList();
+    List<WikiTextNode> sectionContent =
+        content.stream().filter(c -> !c.getType().equals(Section.TYPE)).toList();
+    return new Section(ctx.singleLineValue().TEXT().getText(), 1, sectionContent, subsections);
+  }
+
+  @Override
+  public Section visitSectionLevelTwo(WikiTextParser.SectionLevelTwoContext ctx) {
+    List<WikiTextNode> content = ctx.sectionTwoContent().stream().map(this::visit).toList();
+    List<Section> subsections =
+        content.stream()
+            .filter(c -> c.getType().equals(Section.TYPE))
+            .map(Section.class::cast)
+            .toList();
+    List<WikiTextNode> sectionContent =
+        content.stream().filter(c -> !c.getType().equals(Section.TYPE)).toList();
+    return new Section(ctx.singleLineValue().TEXT().getText(), 2, sectionContent, subsections);
+  }
+
+  @Override
+  public Section visitSectionLevelThree(WikiTextParser.SectionLevelThreeContext ctx) {
+    List<WikiTextNode> content = ctx.sectionThreeContent().stream().map(this::visit).toList();
+    List<Section> subsections =
+        content.stream()
+            .filter(c -> c.getType().equals(Section.TYPE))
+            .map(Section.class::cast)
+            .toList();
+    List<WikiTextNode> sectionContent =
+        content.stream().filter(c -> !c.getType().equals(Section.TYPE)).toList();
+    return new Section(ctx.singleLineValue().TEXT().getText(), 3, sectionContent, subsections);
+  }
+
+  @Override
+  public Section visitSectionLevelFour(WikiTextParser.SectionLevelFourContext ctx) {
+    List<WikiTextNode> content = ctx.sectionFourContent().stream().map(this::visit).toList();
+    List<Section> subsections =
+        content.stream()
+            .filter(c -> c.getType().equals(Section.TYPE))
+            .map(Section.class::cast)
+            .toList();
+    List<WikiTextNode> sectionContent =
+        content.stream().filter(c -> !c.getType().equals(Section.TYPE)).toList();
+    return new Section(ctx.singleLineValue().TEXT().getText(), 4, sectionContent, subsections);
+  }
+
+  @Override
+  public Section visitSectionLevelFive(WikiTextParser.SectionLevelFiveContext ctx) {
+    List<WikiTextNode> content = ctx.sectionFiveContent().stream().map(this::visit).toList();
+    List<Section> subsections =
+        content.stream()
+            .filter(c -> c.getType().equals(Section.TYPE))
+            .map(Section.class::cast)
+            .toList();
+    List<WikiTextNode> sectionContent =
+        content.stream().filter(c -> !c.getType().equals(Section.TYPE)).toList();
+    return new Section(ctx.singleLineValue().TEXT().getText(), 5, sectionContent, subsections);
+  }
+
+  @Override
+  public Section visitSectionLevelSix(WikiTextParser.SectionLevelSixContext ctx) {
+    List<WikiTextNode> sectionContent = ctx.sectionContent().stream().map(this::visit).toList();
+    return new Section(ctx.singleLineValue().TEXT().getText(), 6, sectionContent, List.of());
+  }
+
+  @Override
+  public WikiTextNode visitSectionContent(WikiTextParser.SectionContentContext ctx) {
+    return super.visitSectionContent(ctx);
   }
 }
