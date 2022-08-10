bc3ec0a6d35bb0c9e655be32a3c5160111e06477
Lucas Kjaero <lucas@lucaskjaero.com>
2022-08-10 10:58:28 +0200

    Test the library

--- lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/WikiTextParser.java
+++ lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/WikiTextParser.java
@@ -3,12 +3,8 @@ package com.lucaskjaerozhang.wikitext_parser;
 import com.lucaskjaerozhang.wikitext_parser.objects.Article;
 import com.lucaskjaerozhang.wikitext_parser.parse.SetupParse;
 
-public class Library {
-  public boolean someLibraryMethod() {
-    return true;
-  }
-
-  public Article parseWikiText(String inputText) {
+public class WikiTextParser {
+  public static Article parse(String inputText) {
     Article article = (Article) SetupParse.visitTreeFromText(inputText);
     System.out.println(article.getType());
     return article;
