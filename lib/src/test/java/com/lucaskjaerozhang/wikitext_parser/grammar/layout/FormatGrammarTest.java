ea46cbc2c6d41a874b15e4855b3e90068083b36a
Lucas Kjaero <lucas@lucaskjaero.com>
2022-08-14 00:42:20 +0200

    Test bold and italics

---
+++ lib/src/test/java/com/lucaskjaerozhang/wikitext_parser/grammar/layout/FormatGrammarTest.java
@@ -0,0 +1,76 @@
+package com.lucaskjaerozhang.wikitext_parser.grammar.layout;
+
+import com.lucaskjaerozhang.wikitext_parser.Parser;
+import com.lucaskjaerozhang.wikitext_parser.grammar.WikiTextLexer;
+import com.lucaskjaerozhang.wikitext_parser.grammar.WikitextGrammarBaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * Tests markup elements from https://en.wikipedia.org/wiki/Help:Wikitext#Layout
+ *
+ * <p>Tests both the lexer and parser at the same time because we only care that the grammar is
+ * correct.
+ */
+class FormatGrammarTest extends WikitextGrammarBaseTest {
+  @Test
+  void italicTextIsRecognized() {
+    final String italicText = "''italic''";
+    testLexerTokenTypes(
+        italicText,
+        Arrays.asList(
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.TEXT,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.EOF));
+
+    Assertions.assertEquals(
+        "<article><italic>italic</italic></article>", Parser.parseToString(italicText));
+  }
+
+  @Test
+  void boldTextIsRecognized() {
+    final String boldText = "'''bold'''";
+    testLexerTokenTypes(
+        boldText,
+        Arrays.asList(
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.TEXT,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.EOF));
+
+    Assertions.assertEquals("<article><bold>bold</bold></article>", Parser.parseToString(boldText));
+  }
+
+  @Test
+  void boldAndItalicTextIsRecognized() {
+    final String boldText = "'''''bolditalic'''''";
+    testLexerTokenTypes(
+        boldText,
+        Arrays.asList(
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.TEXT,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.SINGLE_QUOTE,
+            WikiTextLexer.EOF));
+
+    Assertions.assertEquals(
+        "<article><bold><italic>bolditalic</italic></bold></article>",
+        Parser.parseToString(boldText));
+  }
+}
