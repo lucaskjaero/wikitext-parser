c235c00776adb79c5b31fea8ea5bcb89871610d5
Lucas Kjaero <lucas@lucaskjaero.com>
2022-08-16 00:15:11 +0200

    Do I need to invoke this from the command line?

--- lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/parse/SetupParse.java
+++ lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/parse/SetupParse.java
@@ -7,7 +7,7 @@ import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-/** Here we invoke antlr to build the parse tree */
+/** Here we invoke antlr to build the parse tree. */
 public class SetupParse {
   public static WikiTextLexer getLexerFromText(String text, ANTLRErrorListener listener) {
     WikiTextLexer lexer = new WikiTextLexer(CharStreams.fromString(text));
@@ -15,6 +15,13 @@ public class SetupParse {
     return lexer;
   }
 
+  /**
+   * Builds the parse tree
+   *
+   * @param text The text to parse
+   * @param listener An error listener to react to parse problems
+   * @return The parse tree.
+   */
   public static WikiTextParser getParserFromText(String text, ANTLRErrorListener listener) {
     WikiTextParser parser =
         new WikiTextParser(new CommonTokenStream(getLexerFromText(text, listener)));
