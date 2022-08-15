2a1a4e5ddff69889638bc62d43a0a2f33ac66cf8
Lucas Kjaero <lucas@lucaskjaero.com>
2022-08-16 00:00:18 +0200

    This formatter can be pretty annoying

--- lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/ast/link/WikiLinkNamespaceComponent.java
+++ lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/ast/link/WikiLinkNamespaceComponent.java
@@ -1,7 +1,7 @@
 package com.lucaskjaerozhang.wikitext_parser.ast.link;
 
-import com.lucaskjaerozhang.wikitext_parser.metadata.WikiConstants;
 import com.lucaskjaerozhang.wikitext_parser.ast.base.WikiTextElement;
+import com.lucaskjaerozhang.wikitext_parser.metadata.WikiConstants;
 import java.util.Locale;
 
 public class WikiLinkNamespaceComponent implements WikiTextElement {
