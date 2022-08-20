678c8265bc04cbb60f44b4d0b125369354d0b004
Lucas Kjaero <lucas@lucaskjaero.com>
2022-08-20 12:57:02 +0200

    Tags for horizontal rule

--- lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/ast/sections/HorizontalRule.java
+++ lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/ast/sections/HorizontalRule.java
@@ -2,6 +2,11 @@ package com.lucaskjaerozhang.wikitext_parser.ast.sections;
 
 import com.lucaskjaerozhang.wikitext_parser.ast.base.WikiTextLeafNode;
 
+/**
+ * A horizontal rule.<br>
+ * WikiText: ----<br>
+ * XML: horizontalRule
+ */
 public class HorizontalRule extends WikiTextLeafNode {
   public static final String XML_TAG = "horizontalRule";
 
