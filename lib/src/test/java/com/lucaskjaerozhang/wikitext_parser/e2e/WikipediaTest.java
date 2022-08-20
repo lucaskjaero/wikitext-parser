d320ae2764d91505852771b40ab8d4e4c11de378
Lucas Kjaero <lucas@lucaskjaero.com>
2022-08-20 10:27:08 +0200

    Add XML

--- lib/src/test/java/com/lucaskjaerozhang/wikitext_parser/e2e/WikipediaTest.java
+++ lib/src/test/java/com/lucaskjaerozhang/wikitext_parser/e2e/WikipediaTest.java
@@ -36,7 +36,19 @@ class WikipediaTest extends WikitextBaseTest {
                 {{Law-term-stub}}
 
                 """;
-    String xml = "";
+    String xml =
+        """
+            <article><template name='Short description'><parameter value='Delay or suspension of an activity or a law' /></template>
+            <template name='more citations needed'><parameter key='date' value='April 2009' /></template>
+            A <bold>moratorium</bold> is a delay or suspension of an activity or a law. In a <wikilink article='legal'>legal</wikilink> context, it may refer to the temporary suspension of a law to allow a legal challenge to be carried out.
+
+            For example, <wikilink article='animal rights'>animal rights</wikilink> activists and <wikilink article='Conservation movement'>conservation</wikilink> authorities may request fishing or hunting moratoria to protect <wikilink article='endangered'>endangered</wikilink> or threatened animal species.These delays, or suspensions, prevent people from hunting or fishing the animals in discussion.
+
+            Another instance is a delay of legal obligations or payment (<italic><wikilink article='debt moratorium'>debt moratorium</wikilink></italic>). A legal official can order <template name='clarify'><parameter key='text' value='a delay of payment' /><parameter key='date' value='December 2015' /></template> due to extenuating circumstances, which render one party incapable of paying another.<ref><template name='cite web'><parameter key='url' value='http://dictionary.reference.com/browse/moratorium?s=t' /><parameter key='title' value='definition of moratorium' /><parameter key='author' value='dictionary.com' /><parameter key='work' value='dictionary.com' /></template></ref>
+
+            <section level='2' title='See also'>
+            <template name='wiktionary'><parameter value='moratorium' /></template>
+            <list type='unordered'><listItem level='1'><wikilink article='Justice delayed is justice denied'>Justice delayed is justice denied</wikilink></listItem><listItem level='1'><wikilink article='2010 U.S. Deepwater Drilling Moratorium'>2010 U.S. Deepwater Drilling Moratorium</wikilink></listItem><listItem level='1'><wikilink article='Moratorium to End the War in Vietnam'>Moratorium to End the War in Vietnam</wikilink></listItem><listItem level='1'><wikilink article='UN moratorium on the death penalty'>UN moratorium on the death penalty</wikilink></listItem></list></section></article>""";
     testTranslation(article, xml);
   }
 }
