f757001f0f2bf5697939151fc47a0782c0cd124f
Lucas Kjaero <lucas@lucaskjaero.com>
2022-08-20 19:44:12 +0200

    All sections nodes have docs

--- lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/ast/sections/Text.java
+++ lib/src/main/java/com/lucaskjaerozhang/wikitext_parser/ast/sections/Text.java
@@ -4,10 +4,16 @@ import com.lucaskjaerozhang.wikitext_parser.ast.base.TreeConstructionContext;
 import com.lucaskjaerozhang.wikitext_parser.ast.base.WikiTextElement;
 import com.lucaskjaerozhang.wikitext_parser.ast.base.WikiTextNode;
 
+/** Plain text that is not formatted in any way. */
 public class Text extends WikiTextNode implements WikiTextElement {
   public static final String XML_TAG = "text";
   private final String content;
 
+  /**
+   * Creates a text node
+   *
+   * @param content The text within the node.
+   */
   public Text(String content) {
     this.content = content;
   }
@@ -27,6 +33,11 @@ public class Text extends WikiTextNode implements WikiTextElement {
     /* Nothing to pass */
   }
 
+  /**
+   * Get the content within the node.
+   *
+   * @return The content.
+   */
   public String getContent() {
     return content;
   }
