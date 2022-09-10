c1823800f88deb3df74fa98c8c709b2fcfd14168
Lucas Kjaero <lucas@lucaskjaero.com>
2022-09-11 00:30:15 +0200

    Enable jupiter and run it

--- preprocessor/src/test/java/com/lucaskjaerozhang/wikitext_parser/preprocess/e2e/JupiterTest.java
+++ preprocessor/src/test/java/com/lucaskjaerozhang/wikitext_parser/preprocess/e2e/JupiterTest.java
@@ -2,13 +2,13 @@ package com.lucaskjaerozhang.wikitext_parser.preprocess.e2e;
 
 import org.junit.jupiter.api.Test;
 
-public class JupiterTest extends PreprocessorEndToEndTest {
+class JupiterTest extends PreprocessorEndToEndTest {
   public JupiterTest() {
     super("wikipedia", "en", "jupiter");
   }
 
   @Test
   void jupiterTest() {
-    //        endToEndTest("Jupiter");
+    endToEndTest();
   }
 }
