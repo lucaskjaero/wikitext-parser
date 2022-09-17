9d7d0056b6ebc05586cf73fdbc3ee6c9d934d9dd
Lucas Kjaero <lucas@lucaskjaero.com>
2022-09-17 23:37:44 +0200

    Test to update things

--- preprocessor/src/test/java/com/lucaskjaerozhang/wikitext_parser/preprocess/e2e/QingdaoTest.java
+++ preprocessor/src/test/java/com/lucaskjaerozhang/wikitext_parser/preprocess/e2e/QingdaoTest.java
@@ -14,7 +14,24 @@ class QingdaoTest extends PreprocessorEndToEndTest {
 
   @Test
   void nsReferences() {
-    //        testPreprocessorWithFile("{{nsreferences|group=註}}", "nsreferences");
+    testPreprocessorWithFile("{{nsreferences|group=註}}", "nsreferences");
+  }
+
+  @Test
+  void error() {
+    String result =
+        "<module name='Error'><argument>error</argument><argument>引用錯誤：group屬性不能為空</argument><argument>tag=</argument></module>";
+    // Error template
+    testPreprocessorWithString("{{error|引用錯誤：group屬性不能為空}}", result);
+    // Resolves to this.
+    testPreprocessorWithString("{{#invoke:Error|error|引用錯誤：group屬性不能為空|tag=}}", result);
+  }
+
+  @Test
+  void ifNotEq() {
+    testPreprocessorWithString(
+        "<div id=\"references-NoSpaceReferences\">{{#tag:ref|<div style=\"margin-left:-2.7em;margin-bottom:-1.5em\" class=\"noprint\">註:</div>|group=註\u2060|follow=NoSpaceReferences_Prefix_{{#time:U}}}}{{#tag:references|{{{1}}}|group=註\u2060}}|{{error|引用錯誤：group屬性不能為空}}",
+        "");
   }
 
   @Test
