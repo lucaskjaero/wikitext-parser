8fc9bbbe4eb06393611b9acfac3774d0752f2202
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2026-08-01 16:52:09 +0200

    Break tests down into higher granularity

--- preprocessor/src/test/java/com/lucaskjaerozhang/wikitext_parser/preprocess/e2e/MoratoriumTest.java
+++ preprocessor/src/test/java/com/lucaskjaerozhang/wikitext_parser/preprocess/e2e/MoratoriumTest.java
@@ -10,11 +10,6 @@ class MoratoriumTest extends PreprocessorEndToEndTest {
     super("wikipedia", "en", "Moratorium_(law)");
   }
 
-  @Test
-  @Disabled("Whole-article e2e fixture disabled while preprocessor output contract is being iterated.")
-  void moratoriumTest() {
-    endToEndTest();
-  }
 
   /** Short description breakdown */
   @Test
@@ -30,15 +25,13 @@ class MoratoriumTest extends PreprocessorEndToEndTest {
         {{Short description|Delay or suspension of an activity or a law}}
         {{more citations needed|date=April 2009}}
         A '''moratorium''' is a delay or suspension of an activity or a law. In a [[legal]] context, it may refer to the temporary suspension of a law to allow a legal challenge to be carried out.
-        """
-            .stripTrailing(),
+        """,
         """
         <div class="shortdescription nomobile noexcerpt noprint searchaux" style="display:none">Delay or suspension of an activity or a law{{SHORTDESC:Delay or suspension of an activity or a law|}}</div>[[Category:<module name='pagetype'><argument>main</argument></module> with short description]]<module name='Check for unknown parameters'><argument>check</argument><argument>unknown=</argument><argument>preview=Page using [[Template:Short description]] with unknown parameter "_VALUE_"</argument><argument>ignoreblank=y</argument><argument>1</argument><argument>2</argument><argument>pagetype</argument><argument>bot</argument><argument>plural</argument></module><ifexpr><conditional><module name='String'><argument>len</argument><argument>Delay or suspension of an activity or a law</argument></module>>100</conditional><ifTrue>[[Category:<module name='pagetype'><argument>main</argument></module> with long short description]]</ifTrue><ifFalse></ifFalse></ifexpr>
         <module name='Unsubst'><argument></argument><argument>date=__DATE__</argument><argument>$B=
         <module name='Message box'><argument>ambox</argument></module></argument></module>
         A '''moratorium''' is a delay or suspension of an activity or a law. In a [[legal]] context, it may refer to the temporary suspension of a law to allow a legal challenge to be carried out.
-        """
-            .stripTrailing());
+        """);
   }
 
   @Test
@@ -55,8 +48,7 @@ class MoratoriumTest extends PreprocessorEndToEndTest {
         """
         Another instance is a delay of legal obligations or payment (''[[debt moratorium]]''). A legal official can order <module name='Unsubst'><argument></argument><argument>date=__DATE__</argument><argument>$B=
         <span class="cleanup-needed-content" style="padding-left:0.1em; padding-right:0.1em; color:#595959; border:1px solid #DDD;">a delay of payment</span><module name='Category handler'><argument>main</argument></module><module name='Category handler'><argument>main</argument></module><sup class="noprint Inline-Template " style="margin-left:0.1em; white-space:nowrap;">&#91;<i>[[Wikipedia:Cleanup|<span title="<module name='String'><argument>replace</argument><argument>source=Wikipedia:Cleanup</argument><argument>"</argument><argument>&quot;</argument><argument>plain=true</argument><argument>count=</argument></module><nowiki/> (December 2015)">clarification needed</span>]]</i>&#93;</sup></argument></module> due to extenuating circumstances, which render one party incapable of paying another.<ref><module name='citation/CS1'><argument>citation</argument><argument>CitationClass=web</argument></module></ref>
-        """
-            .stripTrailing());
+        """);
   }
 
   @Test
@@ -69,8 +61,7 @@ class MoratoriumTest extends PreprocessorEndToEndTest {
         *[[2010 U.S. Deepwater Drilling Moratorium]]
         *[[Moratorium to End the War in Vietnam]]
         *[[UN moratorium on the death penalty]]
-        """
-            .stripTrailing(),
+        """,
         """
         ==See also==
         <module name='Side box'><argument>main</argument></module>
@@ -78,8 +69,7 @@ class MoratoriumTest extends PreprocessorEndToEndTest {
         *[[2010 U.S. Deepwater Drilling Moratorium]]
         *[[Moratorium to End the War in Vietnam]]
         *[[UN moratorium on the death penalty]]
-        """
-            .stripTrailing());
+        """);
   }
 
   @Test
@@ -89,15 +79,13 @@ class MoratoriumTest extends PreprocessorEndToEndTest {
         ==References==
         {{Reflist}}
         *{{cite NIE|wstitle=Moratorium|year=1905}}
-        """
-            .stripTrailing(),
+        """,
         """
         ==References==
         <templatestyles src="Reflist/styles.css" /><div class="reflist  reflist-{{{group}}} " >
         <references group='' responsive='1'></references></div><module name='Check for unknown parameters'><argument>check</argument><argument>unknown=</argument><argument>preview=Page using [[Template:Reflist]] with unknown parameter "_VALUE_"</argument><argument>ignoreblank=y</argument><argument>1</argument><argument>colwidth</argument><argument>group</argument><argument>liststyle</argument><argument>refs</argument></module>
         *<module name='template wrapper'><argument>wrap</argument><argument>_template=cite encyclopedia</argument><argument>_exclude=display, inline, no-icon, noicon, short, supplement, wstitle, vb, _debug</argument><argument>_reuse=title</argument><argument>year=1905</argument><argument>encyclopedia = [[New International Encyclopedia]]</argument><argument>title=</argument><argument>url=</argument><argument>edition=1st</argument><argument>location=New York</argument><argument>publisher=Dodd, Mead</argument><argument>editor-first=D. C.</argument><argument>editor-last=Gilman</argument><argument>editor-link=Daniel Coit Gilman</argument><argument>editor2-first=H. T.</argument><argument>editor2-last=Peck</argument><argument>editor3-first=F. M.</argument><argument>editor3-last=Colby</argument></module>
-        """
-            .stripTrailing());
+        """);
   }
 
   @Test
@@ -115,15 +103,13 @@ class MoratoriumTest extends PreprocessorEndToEndTest {
 
 
         {{Law-term-stub}}
-        """
-            .stripTrailing(),
+        """,
         """
         [[Category:Legal terminology]]
 
 
         <module name='Asbox'><argument>main</argument></module>
-        """
-            .stripTrailing());
+        """);
   }
 
   @Test
@@ -279,4 +265,116 @@ class MoratoriumTest extends PreprocessorEndToEndTest {
         "{{#if:| <br /><small>{{find sources mainspace|.|}}</small> |{{#if:|{{#ifeq:  |none ||<br /><small>{{find sources mainspace|{{{find}}} }}</small>}}|<br /><small><module name='Find sources'><argument>Find sources mainspace</argument></module></small>}} }}",
         "<br /><small><module name='Find sources'><argument>Find sources mainspace</argument></module></small>");
   }
+  /* Whole-article fixture split into focused logical chunks. */
+
+  @Test
+  void moratoriumArticleLeadTemplates() {
+    testPreprocessorWithString(
+        """
+        {{Short description|Delay or suspension of an activity or a law}}
+        {{more citations needed|date=April 2009}}
+        A '''moratorium''' is a delay or suspension of an activity or a law. In a [[legal]] context, it may refer to the temporary suspension of a law to allow a legal challenge to be carried out.
+        """,
+        """
+        <div class="shortdescription nomobile noexcerpt noprint searchaux" style="display:none">Delay or suspension of an activity or a law{{SHORTDESC:Delay or suspension of an activity or a law|}}</div>[[Category:<module name='pagetype'><argument>main</argument></module> with short description]]<module name='Check for unknown parameters'><argument>check</argument><argument>unknown=</argument><argument>preview=Page using [[Template:Short description]] with unknown parameter "_VALUE_"</argument><argument>ignoreblank=y</argument><argument>1</argument><argument>2</argument><argument>pagetype</argument><argument>bot</argument><argument>plural</argument></module><ifexpr><conditional><module name='String'><argument>len</argument><argument>Delay or suspension of an activity or a law</argument></module>>100</conditional><ifTrue>[[Category:<module name='pagetype'><argument>main</argument></module> with long short description]]</ifTrue><ifFalse></ifFalse></ifexpr>
+        <module name='Unsubst'><argument></argument><argument>date=__DATE__</argument><argument>$B=
+        <module name='Message box'><argument>ambox</argument></module></argument></module>
+        A '''moratorium''' is a delay or suspension of an activity or a law. In a [[legal]] context, it may refer to the temporary suspension of a law to allow a legal challenge to be carried out.
+        """);
+  }
+
+  @Test
+  void moratoriumArticleConservationExample() {
+    testPreprocessorWithString(
+        """
+        For example, [[animal rights]] activists and [[Conservation movement|conservation]] authorities may request fishing or hunting moratoria to protect [[endangered]] or threatened animal species.  These delays, or suspensions, prevent people from hunting or fishing the animals in discussion.
+        """,
+        """
+        For example, [[animal rights]] activists and [[Conservation movement|conservation]] authorities may request fishing or hunting moratoria to protect [[endangered]] or threatened animal species.  These delays, or suspensions, prevent people from hunting or fishing the animals in discussion.
+        """);
+  }
+
+  @Test
+  void moratoriumArticleClarifyCitation() {
+    testPreprocessorWithString(
+        """
+        Another instance is a delay of legal obligations or payment (''[[debt moratorium]]''). A legal official can order {{clarify|text=a delay of payment|date=December 2015}} due to extenuating circumstances, which render one party incapable of paying another.<ref>{{cite web|url=http://dictionary.reference.com/browse/moratorium?s=t|title=definition of moratorium|author=dictionary.com|work=dictionary.com}}</ref>
+        """,
+        """
+        Another instance is a delay of legal obligations or payment (''[[debt moratorium]]''). A legal official can order <module name='Unsubst'><argument></argument><argument>date=__DATE__</argument><argument>$B=
+        <span class="cleanup-needed-content" style="padding-left:0.1em; padding-right:0.1em; color:#595959; border:1px solid #DDD;">a delay of payment</span><module name='Category handler'><argument>main</argument></module><module name='Category handler'><argument>main</argument></module><sup class="noprint Inline-Template " style="margin-left:0.1em; white-space:nowrap;">&#91;<i>[[Wikipedia:Cleanup|<span title="<module name='String'><argument>replace</argument><argument>source=Wikipedia:Cleanup</argument><argument>"</argument><argument>&quot;</argument><argument>plain=true</argument><argument>count=</argument></module><nowiki/> (December 2015)">clarification needed</span>]]</i>&#93;</sup></argument></module> due to extenuating circumstances, which render one party incapable of paying another.<ref><module name='citation/CS1'><argument>citation</argument><argument>CitationClass=web</argument></module></ref>
+        """);
+  }
+
+  @Test
+  void moratoriumArticleSeeAlso() {
+    testPreprocessorWithString(
+        """
+        ==See also==
+        {{wiktionary|moratorium}}
+        *[[Justice delayed is justice denied]]
+        *[[2010 U.S. Deepwater Drilling Moratorium]]
+        *[[Moratorium to End the War in Vietnam]]
+        *[[UN moratorium on the death penalty]]
+        """,
+        """
+        ==See also==
+        <module name='Side box'><argument>main</argument></module>
+        *[[Justice delayed is justice denied]]
+        *[[2010 U.S. Deepwater Drilling Moratorium]]
+        *[[Moratorium to End the War in Vietnam]]
+        *[[UN moratorium on the death penalty]]
+        """);
+  }
+
+  @Test
+  void moratoriumArticleReferences() {
+    testPreprocessorWithString(
+        """
+        ==References==
+        {{Reflist}}
+        *{{cite NIE|wstitle=Moratorium|year=1905}}
+        """,
+        """
+        ==References==
+        <templatestyles src="Reflist/styles.css" /><div class="reflist  reflist-{{{group}}} " >
+        <references group='' responsive='1'></references></div><module name='Check for unknown parameters'><argument>check</argument><argument>unknown=</argument><argument>preview=Page using [[Template:Reflist]] with unknown parameter "_VALUE_"</argument><argument>ignoreblank=y</argument><argument>1</argument><argument>colwidth</argument><argument>group</argument><argument>liststyle</argument><argument>refs</argument></module>
+        *<module name='template wrapper'><argument>wrap</argument><argument>_template=cite encyclopedia</argument><argument>_exclude=display, inline, no-icon, noicon, short, supplement, wstitle, vb, _debug</argument><argument>_reuse=title</argument><argument>year=1905</argument><argument>encyclopedia = [[New International Encyclopedia]]</argument><argument>title=</argument><argument>url=</argument><argument>edition=1st</argument><argument>location=New York</argument><argument>publisher=Dodd, Mead</argument><argument>editor-first=D. C.</argument><argument>editor-last=Gilman</argument><argument>editor-link=Daniel Coit Gilman</argument><argument>editor2-first=H. T.</argument><argument>editor2-last=Peck</argument><argument>editor3-first=F. M.</argument><argument>editor3-last=Colby</argument></module>
+        """);
+  }
+
+  @Test
+  void moratoriumArticleAuthorityControl() {
+    testPreprocessorWithString(
+        """
+        {{Authority control}}
+        """,
+        """
+        <module name='Authority control'><argument>authorityControl</argument></module><module name='Check for unknown parameters'><argument>check</argument><argument>arts</argument><argument>state</argument><argument>extralist</argument><argument>ignoreblank=1</argument><argument>showblankpositional=1</argument><argument>unknown=[[Category:Pages using authority control with parameters|_VALUE_]]</argument><argument>preview=Page using [[Template:Authority control]] with "_VALUE_", please move this to Wikidata if possible</argument></module>
+        """);
+  }
+
+  @Test
+  void moratoriumArticleFooterCategoriesAndStub() {
+    testPreprocessorWithString(
+        """
+        [[Category:Legal terminology]]
+
+        {{Law-term-stub}}
+        """,
+        """
+        [[Category:Legal terminology]]
+
+        <module name='Asbox'><argument>main</argument></module>
+        """);
+  }
+
+  /* Whole-article smoke test kept disabled while paragraph contracts are refined. */
+
+  @Test
+  @Disabled("Whole-article e2e fixture disabled while preprocessor output contract is being iterated.")
+  void moratoriumTest() {
+    endToEndTest();
+  }
+
 }
